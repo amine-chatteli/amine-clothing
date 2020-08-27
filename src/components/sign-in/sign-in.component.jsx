9b8bd746b021a874b244179f0d6a81ce93525bda
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2020-08-27 18:51:11 +0100

    form input reusable component + styles

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './sign-in.styles.scss';
-
+import FormInput from '../../components/form-input/form-input.component';
 
 class SignIn extends Component {
     constructor(props) {
@@ -29,19 +29,21 @@ class SignIn extends Component {
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form onSubmit={this.handleSubmit}>
-                    <label>Email</label>
-                    <input type="email"
+                    
+                    <FormInput type="email"
                         name="email"
                         value={this.state.email}
                         required
-                        onChange={this.handleChange} />
-                    <label>Password</label>
-                    <input
+                        handleChange={this.handleChange}
+                        label="email" />
+                   
+                    <FormInput
                         type="password"
                         name="password"
                         value={this.state.password}
                         required
-                        onChange={this.handleChange} />
+                        handleChange={this.handleChange} 
+                        label="password" />
 
                     <input type="submit" value="Submit Form" />
                 </form>
