80d47cb9854ab9cca0a63a0667af321d0752d1b6
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2020-08-27 04:07:04 +0100

    collection preview, collection item , styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import HomePage from "./pages/homepage/homepage.component"; 
-import {Route,Switch} from 'react-router-dom';
+import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
-      <Route exact path ='/' component={HomePage}/>
-      <Route exact path ='/hats' />
-
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
-    </div>
+    </div> 
   );
 }
 
