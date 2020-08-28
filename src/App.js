e24a5c15aaa041bc0f4e9d39ab981deddbec2278
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2020-08-28 04:31:32 +0100

    setting current user to user state from firebase

--- src/App.js
+++ src/App.js
@@ -5,19 +5,39 @@ import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPAge from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentUser: null
+    }
+  }
+  unsubscribeFromAuth
+  componentDidMount() {
+    //setting our local state to user state provided by firebase subscription
+    auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user })
+      console.log(user);
+    })
+  }
+  componentWillUnmount() {
+    //close firebase subscription to avoid memory leaks
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/signin' component={SignInAndSignUpPAge} />
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SignInAndSignUpPAge} />
-
-      </Switch>
-    </div>
-  );
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
