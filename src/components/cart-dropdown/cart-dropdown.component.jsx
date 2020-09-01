b961ca42e3b2fcd55e663bb882db8b94a61bb0b6
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2020-09-02 00:03:25 +0100

    remove item from cart on checkout

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,9 +4,10 @@ import CartItem from '../cart-item/cart-item.component';
 import { connect } from 'react-redux';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { withRouter } from 'react-router-dom';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems,history}) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className="cart-dropdown">
         <div className="cart-items" >
             {cartItems.length ?
@@ -15,10 +16,14 @@ const CartDropdown = ({ cartItems,history}) => (
                 <span className="empty-message">You cart is empty</span>
             }
         </div>
-        <CustomButton onClick={()=>history.push('/checkout')}>CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            history.push('/checkout');
+            dispatch(toggleCartHidden())
+        }}>
+        CHECKOUT</CustomButton>
     </div>
 );
-
+ 
 
 const mapStateToProps = state => ({
     cartItems: selectCartItems(state)
