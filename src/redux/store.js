ec0edcf94c7eaf650e9107c9330a3352dae7fdbc
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2020-09-02 09:43:24 +0100

    persisting cart to local storage using redux-persist library

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
+import {persistStore} from 'redux-persist';
 
 //creating a table of middlewares to add on it whenever we need a new middleware (scalability)
 const middlewares =[logger];
-const store =createStore(rootReducer,applyMiddleware(...middlewares));
-
-export default store;
+export const store =createStore(rootReducer,applyMiddleware(...middlewares));
+export const persistor =persistStore(store);
+export default {store,persistor};
