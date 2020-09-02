5b17e7fb65deac8348803033564dfdc8fbca8e22
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2020-09-02 13:21:56 +0100

    load directory items from redux reducer instead of local state

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,5 +1,6 @@
 import userReducer from './user/user.reducer';
-import cartReducer from './cart/cart.reducer'
+import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
@@ -11,7 +12,8 @@ const persistConfig = {
 }
 const rootReducer = combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory:directoryReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
