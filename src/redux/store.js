15e0ec204d5bb1ffd4251aa553529c18eab99416
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2020-09-28 12:46:37 +0100

    fetching data through async redux actions instead of componentDidMount

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import {createStore, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 import {persistStore} from 'redux-persist';
+import thunk from 'redux-thunk';
 
 //creating a table of middlewares to add on it whenever we need a new middleware (scalability)
-const middlewares =[logger];
+
+const middlewares =[logger,thunk];
 export const store =createStore(rootReducer,applyMiddleware(...middlewares));
 export const persistor =persistStore(store);
 export default {store,persistor};
