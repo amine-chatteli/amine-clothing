9b59bcad2fa9a8d468ead6ed19dfb296ddee7d97
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2020-11-03 09:39:27 +0100

    Updates

---
+++ precache-manifest.06c2dc6cc13e8ce9f740a273b44b3ec3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9942b919fbc4b6c76410e7e45a2a459",
+    "url": "/amine-clothing/index.html"
+  },
+  {
+    "revision": "d0ece11797a075d48409",
+    "url": "/amine-clothing/static/css/main.86d7cd1e.chunk.css"
+  },
+  {
+    "revision": "0104e0b8c963237e22c3",
+    "url": "/amine-clothing/static/js/2.ab775881.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/amine-clothing/static/js/2.ab775881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0ece11797a075d48409",
+    "url": "/amine-clothing/static/js/main.1551b615.chunk.js"
+  },
+  {
+    "revision": "a23329a9b9a58c78f1b2",
+    "url": "/amine-clothing/static/js/runtime-main.767ad530.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/amine-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/amine-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
