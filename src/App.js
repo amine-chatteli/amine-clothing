83b6a8425d4aec8fb6be9993207eb9addfdca361
amine-chatteli <45702306+amine-chatteli@users.noreply.github.com>
2020-11-03 10:32:26 +0100

    fixing routing bug

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ class App extends React.Component {
       <div>
         <Header />
         <Switch>
-          <Route exact path='/' component={HomePage} />
+          <Route  path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
 
